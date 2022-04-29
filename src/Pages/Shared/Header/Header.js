300222fdc9ab3c6f08869742e9864dbc999b288e
zannatreya <zannatulbintabahar@yahoo.com>
2022-04-29 11:23:43 +0600

    header & footer added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
